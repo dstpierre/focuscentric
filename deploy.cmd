ced1222333bde0a1245ad82006223be017ceba89
Dominic St-Pierre <dominicstpierre@gmail.com>
2015-03-07 16:13:06 -0500

    tired of this already, still does not work

--- deploy.cmd
+++ deploy.cmd
@@ -12,10 +12,10 @@ SET FOLDER=%date:~-4,4%%date:~-10,2%%date:~-7,2%_%hr%%time:~3,2%%time:~6,2%
 MKDIR %NEWHOME%\site\wwwroot\gopath\src\%FOLDER%
 
 xcopy %DEPLOYMENT_SOURCE% %NEWHOME%\site\wwwroot\gopath\src\%FOLDER% /Y
-xcopy /r %DEPLOYMENT_SOURCE%\Web.Config %NEWHOME%\site\wwwroot\Web.Config* /Y
+xcopy /r %DEPLOYMENT_SOURCE%\Web.Config %NEWHOME%\site\wwwroot\Web.Config /Y
 
-xcopy %DEPLOYMENT_SOURCE%\content\ %NEWHOME%\site\wwwroot\content\ /S /Y
-xcopy %DEPLOYMENT_SOURCE%\views\ %NEWHOME%\site\wwwroot\views\ /S /Y
+xcopy %DEPLOYMENT_SOURCE%\content\* %NEWHOME%\site\wwwroot\content\ /S /Y
+xcopy %DEPLOYMENT_SOURCE%\views\* %NEWHOME%\site\wwwroot\views\ /S /Y
 
 SET GOPATH=%NEWHOME%\site\wwwroot\gopath
 SET GOROOT=%NEWHOME%\site\wwwroot\go
@@ -23,8 +23,8 @@ SET PATH=%PATH%;%GOPATH%\bin;%NEWHOME%\site\wwwroot\go\bin
 
 go get %FOLDER%
 
-xcopy %NEWHOME%\site\wwwroot\gopath\bin\%FOLDER%.exe %NEWHOME%\site\wwwroot\ /Y
-
-sed -i 's/GOAPPBINARY/%FOLDER%.exe/g' %NEWHOME%\site\wwwroot\Web.Config
-
 powershell "stop-process (Get-Process w3wp | Sort-Object ws | Select -first 1).Id"
+
+xcopy %NEWHOME%\site\wwwroot\gopath\bin\%FOLDER%.exe %NEWHOME%\site\wwwroot\app.exe /Y
+
+REM sed -i 's/GOAPPBINARY/%FOLDER%.exe/g' %NEWHOME%\site\wwwroot\Web.Config
